da1dd2e7e62d40dac327256cabbe9fef2bd406eb
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2021-02-03 22:56:03 +0100

    Install all colors variants of Orchis GTK theme.

--- run_once_install_orchis_theme.sh
+++ run_once_install_orchis_theme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-TEMPDIR="$(mktemp --directory --tmpdir '-chezmoi-XXXXXXXXXX-install-orchis-theme')"
+TEMPDIR="$(mktemp --directory --tmpdir 'chezmoi-XXXXXXXXXX-install-orchis-theme')"
 cd "$TEMPDIR"
 git clone "https://github.com/vinceliuice/Orchis-theme" "$TEMPDIR"
-"$TEMPDIR/install.sh" --dest "${XDG_DATA_HOME:-$HOME/.local/share}/themes"
+"$TEMPDIR/install.sh" --dest "${XDG_DATA_HOME:-$HOME/.local/share}/themes" --theme all
 rm -rf "$TEMPDIR"
