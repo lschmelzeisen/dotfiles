e8b3ef070b1dcbc54a4e938b76d6ed26f92b62dc
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2021-03-31 16:57:17 +0200

    Reorganize .bashrc and change reboot scripts to be skipable.

--- run_once_01_restart.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-echo "Rebooting, so that environment variables are loaded before running scripts..."
-sudo systemctl reboot
--- run_once_99_restart.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-echo "Rebooting, so that all performed changes are loaded..."
-sudo systemctl reboot
